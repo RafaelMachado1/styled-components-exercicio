78cb9ba0af126cc82b78842adc745aba360bcc6f
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 16:53:49 -0300

    exercicios-conluidos

---
+++ src/Componentes/Footer/Footer.js
@@ -0,0 +1,12 @@
+import react from "react"
+import { Footer1 } from "./style"
+
+
+export function Footer(){
+    
+    return(
+        <Footer1>
+          <h4>Oi! Eu moro no footer!</h4>
+        </Footer1>
+        )
+}
